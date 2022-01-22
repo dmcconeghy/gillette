b1302b7d9ea54e6593ee2d1663a8d242dfc9372d
Dave McConeghy <david.mcconeghy@gmail.com>
2022-01-22 12:29:03 -0500

    Removes unneeded validated for inputted product arrays

--- src/Products/ProductTable.js
+++ src/Products/ProductTable.js
@@ -3,7 +3,7 @@ import ProductCard from './ProductCard'
 
 
 //By default the ProductTable returns all 20 items in the Fake Store API.
-// Currently stateless, but since API calls populate empty props, will need to be in state.  
+// Currently stateless, but since API calls populate empty props, may need to be in state.  
 function ProductTable({ productIdArray = null }) {
 
     // //initialize the state of products as null to preserve the return ternary 
@@ -12,30 +12,15 @@ function ProductTable({ productIdArray = null }) {
     function GenerateSequentialArray() {
         return Array.apply(null, {length: 20}).map(Number.call, Number);
     }
-
-    const allProductsArray = GenerateSequentialArray()
-
-    // Our product Ids can't be less than 1 or greater than 20
-    function ValidateArray(unfilteredArray) {
-        //null arrays return all products by default
-        if (unfilteredArray === null){
-            return 
-        } else return unfilteredArray.filter(value => { return value > 1 && value < 21;});
-    }
-    
-    const validatedIdArray = ValidateArray(productIdArray)
-
-    // set the state with the validated array
-    // setProducts(validatedIdArray)
     
      return (
          
          <div className="ProductTable">
              {productIdArray ? (
-                 (validatedIdArray.map(idx => 
+                 (productIdArray.map(idx => 
                     (<ProductCard number={idx} key={idx}/>)))
                  )
-            : (allProductsArray.map(idx => 
+            : (GenerateSequentialArray().map(idx => 
                 (<ProductCard number={idx+1} key={idx+1}/>)))
              } 
              
