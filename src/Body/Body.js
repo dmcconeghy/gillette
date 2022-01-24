c22ad1d4a3eee1f391d039a2157e9d634bc64b10
Dave McConeghy <david.mcconeghy@gmail.com>
2022-01-23 23:39:42 -0500

    Updates Filter component with new name Category

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,8 +1,10 @@
-import { useContext } from 'react'
 import '../styles/Body.css'
-import ProductTable from '../Products/ProductTable'
-import Filter from '../Search/Filter'
+import { useContext } from 'react'
 import { SearchContext } from '../Search/SearchContext'
+import ProductTable from '../Products/ProductTable'
+import Category from '../Search/Category'
+import Sort from '../Search/Sort'
+
 
 function Body () {
 
@@ -10,17 +12,26 @@ function Body () {
 
  return (
     <div className="BodyWrapper">
-        <div className='SearchFilters'>
-        <div className="Filters">
-                <Filter category= "Electronics" />
-                <Filter category= "Jewelery" />
-                <Filter category= "Men's Clothing" />
-                <Filter category= "Women's Clothing" />
-            </div>
+        <div className="Sidebar">
+            <div> Category filter box component</div>
+            <div> Price sort buttons </div>
+            <Sort direction="asc"/>
+            <Sort direction="desc"/>
         </div>
 
-        <div className="ProductTable">
-            <ProductTable productIdArray = { searchResults } /> 
+        <div className="Content">
+            <div className='SearchFilters'>
+            <div className="Filters">
+                    <Category category= "Electronics" />
+                    <Category category= "Jewelery" />
+                    <Category category= "Men's Clothing" />
+                    <Category category= "Women's Clothing" />
+                </div>
+            </div>
+
+            <div className="ProductTable">
+                <ProductTable productsObject = { searchResults } /> 
+            </div>
         </div>
        
     </div>
