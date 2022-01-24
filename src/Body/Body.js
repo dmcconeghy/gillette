f9c0c41ff752bd013214978c23ac1cd76cd15708
Dave McConeghy <david.mcconeghy@gmail.com>
2022-01-23 19:51:05 -0500

    Changes div structure around filter buttons

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,8 +1,7 @@
-import { useState, useContext } from 'react'
+import { useContext } from 'react'
 import '../styles/Body.css'
-import Search from '../Search/Search';
-import ProductTable from '../Products/ProductTable';
-import Filter from '../Search/Filter';
+import ProductTable from '../Products/ProductTable'
+import Filter from '../Search/Filter'
 import { SearchContext } from '../Search/SearchContext'
 
 function Body () {
@@ -10,18 +9,16 @@ function Body () {
     const { searchResults }  = useContext(SearchContext)
 
  return (
-     <div className="BodyWrapper">
-         <div className="SearchFilters">
-            <div className="SearchField">
-                
-            </div>
-            <div className="Filters">
+    <div className="BodyWrapper">
+        <div className='SearchFilters'>
+        <div className="Filters">
                 <Filter category= "Electronics" />
                 <Filter category= "Jewelery" />
                 <Filter category= "Men's Clothing" />
                 <Filter category= "Women's Clothing" />
             </div>
         </div>
+
         <div className="ProductTable">
             <ProductTable productIdArray = { searchResults } /> 
         </div>
