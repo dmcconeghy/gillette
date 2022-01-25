2f420d4db02761e25be7a697f9f87d60e267402e
Dave McConeghy <david.mcconeghy@gmail.com>
2022-01-25 16:02:57 -0500

    Implements price sorting. Feature is buggy since the searchResults it generates aren't correctly being updated. Perhaps a conflict with the useState re-rendering issues from SearchHelpers.js

--- src/Search/PriceSort.js
+++ src/Search/PriceSort.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { executeSearch } from './SearchHelpers';
 import { SearchContext } from './SearchContext'
 
@@ -11,67 +11,65 @@ import { SearchContext } from './SearchContext'
 
 // Reducing console clutter temporarily, this is definitely resulting in additional and unwanted renders. 
 function PriceSort(props) {
+
+  const firstUpdate = useRef(true);
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+   
+  });
+
     const {
-        searchTerm, 
-        selectedCategories,
         searchResults,
-        setSearchResults 
+        setSearchResults, 
+        sortAscending,
+        setSortAscending 
       } = useContext(SearchContext)
 
-      // const [isSortedUp, setIsSortedUp] = useState(false)
-      // const [currentResults, setCurrentResults] = useState([])
+      const [isSortedUp, setIsSortedUp] = useState(false)
+      const [currentResults, setCurrentResults] = useState([])
 
 
-      // function handleClick(evt){
-      //   evt.preventDefault()
+      function handleClick(evt){
+        evt.preventDefault()
         
-      //   console.log("You clicked sort by price")
-      //   setIsSortedUp(!isSortedUp);
-      //   console.log("Sort ascending is:", isSortedUp)
-        // console.log("handleclick", searchResults)
-
-        // let sortedResults = searchResults
         
+        setIsSortedUp(!isSortedUp);
+        console.log("You clicked sort by price ascending =", isSortedUp)
 
-        // if (!isSortedUp){
-        //   sortedResults.sort((a,b) => (a.price > b.price) ? 1 : -1)
-        // } else {
-        //   sortedResults.sort((a,b) => (a.price > b.price ) ? -1 : 1)
-        // }
-
-        // setCurrentResults(searchResults)
-      // }
-
-      // console.log(currentResults)
+      }
       
-      // useEffect(() => {
-      //   function sortResults(){
+      useEffect(() => {
+        function sortResults(){
 
-      //     console.log("Sort useEffect fires")
-      //     // let sortedResults = searchResults
+          let sortedResults = searchResults
 
-      //     // if (!isSortedUp){
-      //     //   sortedResults.sort((a,b) => (a.price > b.price) ? 1 : -1)
+          if (!isSortedUp){
+            sortedResults.sort((a,b) => (a.price > b.price) ? 1 : -1)
             
-      //     // } else if (isSortedUp){
-      //     //   sortedResults.sort((a,b) => (a.price > b.price ) ? -1 : 1)
+          } else if (isSortedUp){
+            sortedResults.sort((a,b) => (a.price > b.price ) ? -1 : 1)
 
-      //     // }
+          }
           
-      //     // setSearchResults(sortedResults)
+          setSearchResults(sortedResults)
+         
           
-      //     // console.log("useEffect", searchResults)
-      //     //Why does a change in search results not result in a re-render?
-      //   }
-      //   sortResults()
-      // }, [isSortedUp, searchResults, setSearchResults, currentResults, setCurrentResults])
+          // Why does a change in search results not result in a re-render?
+          // Due to the useState bug in the app, results appear to be correctly sorted on the accidental re-renders. 
+        }
+        sortResults()
+      }, [isSortedUp, searchResults, setSearchResults, currentResults, setSortAscending, setCurrentResults])
+    
     
 
-      //onClick={(evt) => handleClick(evt)}
+      
     
   return (
     <div className="PriceSort">
-        <button >Sort Results By Price&#8593;&#8595;</button>
+        <button onClick={(evt) => handleClick(evt)}>Sort Results By Price&#8593;&#8595;</button>
     </div>
     )
 }
