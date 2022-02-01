81512265230d2af53939d889354203b02f3d9a0d
Dave McConeghy <david.mcconeghy@gmail.com>
2022-02-01 14:19:38 -0500

    SearchForm now only handles the creation of the search input field and the passing of the searchTerm to the global context. Previously SearchForm used a callback with Search.js that was refactored.

--- src/Search/SearchForm.js
+++ src/Search/SearchForm.js
@@ -1,29 +1,54 @@
-import { useState } from "react"
+import { useState, useContext, useEffect } from "react"
 import '../styles/Header.css'
+import { SearchContext } from "../Search/SearchContext"
+import { executeSearch } from "../Search/SearchHelpers"
 
-//The SearchForm component generates the search field and passes user inputted search terms to its parent using search(term). 
+function SearchForm() {
 
-//initalize the searchForm's empty state
-function SearchForm({ search }) {
-  const [term, setTerm] = useState("");
+  const { searchTerm, setSearchTerm, setSearchResults, setSortAscending } = useContext(SearchContext)
+  const [inputtedTerm, setInputtedTerm] = useState("");
 
   //If the contents change, set the term to the state
   function handleChange(evt) {
-    setTerm(evt.target.value);
+    
+    setInputtedTerm(evt.target.value);
+    
   };
 
-  // If the form is submitted pass the term to the search parent and clear the search field
+  //Placing this outside the handleChange 
+  if (inputtedTerm !== ""){
+    console.debug(`SearchForm input: ${inputtedTerm}`)
+  }
+  // If the form is submitted pass the term to the global SearchContext
   function handleSubmit(evt) {
     evt.preventDefault();
-    search(term);
-    setTerm("");
+    setSearchTerm(inputtedTerm)
+    
   }
+
+  //When the term has been set by a form submission, pass it to executeSearch and then set searchResults to equal that query.  
+  useEffect(() => { 
+     async function updateSearchTerm() {
+
+      if(searchTerm !== ""){
+        console.log(`Executing search for "${searchTerm}"`)
+        
+        setSearchResults(await executeSearch(searchTerm))
+        
+        // Not sure where searchTerm should be reset yet.
+        // Putting the reset here means that items can't be price filtered or sorted by category within a term's search results.  
+        // setSearchTerm("")
+      }
+    }
+      updateSearchTerm()
+    },[searchTerm, setSearchResults, setSortAscending, setSearchTerm])
 
   return (
     <label className="SearchField">
-      {console.debug("The SearchField component has rendered")}
+  
+      {console.debug("<SearchForm /> rendered")}
       <form onSubmit={handleSubmit}>
-        <input type="search" value={term} placeholder="Search..." onChange={handleChange} /> 
+        <input type="search" value={inputtedTerm} placeholder="Search..." onChange={handleChange} /> 
         
       </form>
     </label>
