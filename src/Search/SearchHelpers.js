62e643304f2a1a1ab57a1fe318a11a424345e820
Dave McConeghy <david.mcconeghy@gmail.com>
2022-02-01 14:21:00 -0500

    A significant refactor of the logic handling of SearchHelpers reduced errors and almost all errant re-renderings. Handling, in turn, search terms, categories, and price filters, SearchHelpers now appropriately uses the correct logic to combine these three search features when called for.

--- src/Search/SearchHelpers.js
+++ src/Search/SearchHelpers.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-const all_products_URL = "https://fakestoreapi.com/products/";
-const category_URL = "https://fakestoreapi.com/products/category/"
-
 // Searchhelpers is a key logic file for this project used by numerous components.  
 // It takes a searchTerm and/or array of categories and results results from the Fake Store API in variable "filteredResponse". 
 // Its logic looks first for category-alone requests (using the filter-by category buttons, say with all products displayed by default).
@@ -11,117 +8,172 @@ const category_URL = "https://fakestoreapi.com/products/category/"
 // For a refactor it would be helpful to split the logic across the variables and handle each in turn.
 // In this way you could separating and resuse the category url parser or add to the RegExp logic to search multple words or use more advance search operands. 
 
-async function executeSearch(term = "", categories = [], price = [0, -1], ascending) {
+// This array of product objects is fetched at the start of each app load, since this is the top-level of SearchHelpers. 
+// An obvious refactor would be to extract this API call into a component to pull it into the global SearchContext.
+// If we expected the db to change, this fetch might employ a recurring useMemo call. 
 
-    // if (categories === null){categories = []}
-    let search_URL;
-    let filteredResponse; 
+const all_products_URL = "https://fakestoreapi.com/products/";
 
-    // If categories isn't null then we may be dealing with a category filter request
-    // Categories are passed as an array of strings that must be correctly parsed for API URL.
-    // This logic currently assumes that if we're using categories we do not have a search. 
-    // This is presently obstructing filtering within search results, since the searchTerm needs to be passed to the filter and retained until it is set again elsewhere.
-    // Search.js hoists searchTerm into the searchContext, so a code review will be needed to find places where that variable may be being altered uneccessarily. 
-    if (categories !== null && categories.length > 0) {
+async function getAllProducts() {
+    console.debug("Start of search: all product results being fetched.")
 
-        const parsedCategories = categories.map((category) => {
-                if (category === "men's clothing"){
-                     category = "men%27s%20clothing"
-                } 
-                
-                if (category === "women's clothing"){
-                    category= "women%27s%20clothing"
-                } 
-                
-                return category
-            })
+    const searchresponse = await axios({
+        method: 'get',
+        url: `${all_products_URL}`,
+        withCredentials: false,
+    })
+
+    console.debug(`getAllProducts returned ${searchresponse.data.length} products`)
+    return searchresponse.data
+}
+
+//It would be ideal for use to await allProducts here, but we can't use await on the top level. 
+// We could all experiments from WebPack to turn this on or find a way to wrap this in a function.
+// What we want to avoid is having every search call our API. 
+// So we could sessionize or memoize our data or write it to a json file? 
+
+const allProductsPromise = getAllProducts()
+
+
+
+// Our first goal in the app is to display all products as the default landing page. 
+// Our search function has the following default variables globally scoped in components using SearchContext:
+// searchTerm = ""
+// selectedCategories = []
+// priceFilter = [0, -1]
+// On-load we have no search term, no categories, and no price filter.
+// Searches which contain exactly these defaults load the unaltered allProductObjects. 
+// executeSearch's default variables are different, too, since this is not a component
+// Price sort is an on-click option. 
+
+// Our primary search return function is executeSearch, which always returns an array of product objects to be displayed. 
+async function executeSearch(term = "", categories = [], price = [0, -1]){
+
+    // for each new search we want to start with allProducts and empty variables. 
+    // This ensures that changes in the search (expansions or reductions) are handled correctly. 
+    // Otherwise reductions would be be lost if the search got less restrictive. 
+
+    const allProducts = await allProductsPromise
+    // const allProducts = await getAllProducts()
+
+    let searchResultsAccumulator = [] 
+    let searchFilteredProducts = [] 
+    let categoryFilteredProducts = [] 
+    let priceFilteredProducts = []
+    let finalResults = []
     
-    // Now we need to generate search results for each category in the array. 
-    // We'll fetch results with getData, iterate over the categories, and then concat the responses to a single searchResults array. 
+    // This function always takes the full allProducts and returns only products matching the inputted term.
+    function textSearch (term, searchResultsAccumulator) {
 
-    async function getData (category) {
-        search_URL = (category_URL+`${category}`)
-        const categoryresponse = await axios({
-            method: 'get',
-            url: `${search_URL}`,
-            withCredentials: false,
-        })
-        return categoryresponse.data
-    }
-
-    // This should be refcatored to reduce calls. One solution would be to call allProducts once and memoize it. 
-    // Then future searches could either use it as the default or turn it into subsets for search/category/price requests. 
-    // It appears through the console to be requesting the data twice. A useRef hook was added to Category.js to try to avoid this. 
-    async function aggregateData (categoryArray){
-        let data = []
-        for (let i=0; i<categoryArray.length; i++){
-            search_URL= (category_URL+`${categoryArray[i]}`)
-            data = data.concat(await getData(categoryArray[i]))
-        }
-
-        return data
-    }
-        
-    filteredResponse = aggregateData(parsedCategories)
-
-    // returns an array of products 
-    } else {
-        //Keeping this line in the console.log reveals how executeSearch is not handling componentmounting/updating in an ideal way. 
-        // Searches result in brief pops of different searchResults, say between an original product array arriving and then being sorted. 
-        // These problems are most acute in relation to the Category component and its related search functions here in the logic above. 
-        console.log("All product results incoming...")
-        search_URL = all_products_URL
-
-        const searchresponse = await axios({
-            method: 'get',
-            url: `${search_URL}`,
-            withCredentials: false,
-        })
-        
-        //parse just the product item data
-        const catalog = searchresponse.data
-        
-        if (term === "" && (categories === [] || categories === null)){
-            return catalog
-        }
-        //This expression looks for our term in the catalog string
-        // the i flag ignores case
-        // the g flag returns all results, not just the first one
-        // This search has a notable limitation since substring "men" is returned when searching "women". 
+        //Looks for our term in allProducts
+            // the i flag ignores case
+            // the g flag returns all results, not just the first one
+            // This search has two notable limitations:
+            // 1) it handles substring matches poorly and
+            // 2) it does not handle multiple words additively but rather an entire strings.
         const searchExpression = new RegExp('.*' + term + '.*', 'gi')
-      
-        filteredResponse = catalog.filter(item => searchExpression.test(item.title) || searchExpression.test(item.description) || searchExpression.test(item.category))
-    
-        //During testing this is helpul to see data on the given and returned items.
-        // if (filteredResponse.length === 0){
-        //     console.log(`Search for "${term}" returned no results`)
-        // } else {
-        //     console.log(`Search for "${term}" returned ${filteredResponse.length} results`)
-        // }
         
-    } 
+        const textSearchProducts = searchResultsAccumulator.filter(item => searchExpression.test(item.title) || searchExpression.test(item.description) || searchExpression.test(item.category))
 
-    // Filter is very touchy about being given non arrays, which often happens during testing. 
-    if (price[1] !== -1 && filteredResponse){
+        console.debug(`textSearch() returning ${textSearchProducts.length}search results for "${term}"`)
+
+        return textSearchProducts
+    }
+
+    // This function always takes the current accumulator and reduces it to only the matching prices
+    function pricedProducts (price, searchResultsAccumulator){
         
-        const pricedResults = filteredResponse.filter((product) => 
-            product.price >= price[0] && product.price <= price[1]
-        )
+        const pricedProducts = searchResultsAccumulator.filter((product) => 
+            product.price >= price[0] && product.price <= price[1])
 
         console.log(`Prices limited to between results between ${price[0]} and ${price[1]}`)
 
-        filteredResponse = pricedResults
+        return pricedProducts
     }
 
-    if (ascending === true) {
+    // This function always takes the current accumulator and reduces it to only the matching categories
+    function categorizedProducts(categories, searchResultsAccumulator = []){
+            let categoryProducts = []
+
+            for (let c=0; c < categories.length; c++){
+                for (let i=0; i<searchResultsAccumulator.length; i++){
+                    if (searchResultsAccumulator[i].category === categories[c]) { 
+                        categoryProducts.push(searchResultsAccumulator[i])
+                    }
+                }
+            }
+            
+            return categoryProducts
+        }
+
+    // Is everything a default? Return all 20 products sorted by their default productIds.
+    // if not set the searchResultsAccumulator to allProducts
+    if (arguments.length === 0){
+
+        return finalResults = allProducts
+    } else {
+        searchResultsAccumulator = allProducts
+    }
+    
+    // Is there a term?
+    if (term !== ""){
+
+        // look for our term in the accumulator and reduce the results.
+        searchFilteredProducts = textSearch(term, searchResultsAccumulator)
         
-        filteredResponse.sort((a,b) => (a.price > b.price) ? 1 : -1)
-    } else if (ascending === false){
-        filteredResponse.sort((a,b) => (a.price > b.price ) ? -1 : 1)   
-    } 
+        // end of the line for term-only searches
+        if (categories.length === 0 && price[1] === -1){
 
+           return finalResults = searchFilteredProducts
+           
+        // Otherwise update the accumulator
+        // no return as we still check other search variables  
+        } else {
+            searchResultsAccumulator = searchFilteredProducts
+             
+        }
+    }
 
-    return filteredResponse
+    //Are there categories?
+    if (categories.length > 0){
+
+        //Look for our categories in the accumulator and reduce the results
+        categoryFilteredProducts = categorizedProducts(categories, searchResultsAccumulator)
+
+        // end of the line for category-only searches
+        if (term === "" && price[1] === -1){
+            return finalResults = categoryFilteredProducts
+        
+        // Otherwise update the accumulator
+        // no return as we still check other search variables  
+        } else {
+            searchResultsAccumulator = categoryFilteredProducts
+        }
+    }
+
+    // Are we price filtering?
+    if (price[1] !== -1){
+
+        //Check the prices of items in the accumulator and reduce the results
+        priceFilteredProducts = pricedProducts(price, searchResultsAccumulator)
+
+        //End of the line for price-only searches
+        if (term === "" && categories.length === 0){
+           return finalResults = priceFilteredProducts
+        
+        // Otherwise we have our final result already
+        } else {
+            searchResultsAccumulator = priceFilteredProducts
+        }
+
+    }
+    
+    // Haven't returned finalResults yet? Time to pass our accumulator's results to it. 
+    finalResults = searchResultsAccumulator
+
+    console.log(`executeSearch() has finished with ${finalResults.length} final results for searchTerm(${term}), categories(${categories}), price(${price})`)
+
+    return finalResults
 }
 
 export { executeSearch }
