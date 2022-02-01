e6803dd951cea8df8c65cb041b4574bcf2cf151d
Dave McConeghy <david.mcconeghy@gmail.com>
2022-02-01 14:14:40 -0500

    A major rework of the priceFilter. Numerous edge cases were being improperly handled. Only a handful remain, primarily tied to not placing submit of minMax's GO button in a useEffect. Doing so causes updates as users type into the min/max fields, which can be distracting. Code for this option remains and is commented out.

--- src/Search/PriceFilter.js
+++ src/Search/PriceFilter.js
@@ -1,53 +1,124 @@
 import '../styles/Body.css'
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { SearchContext } from './SearchContext';
 import { executeSearch } from './SearchHelpers'
 
 
-// PriceFilter currently executes on all products rather than the specific subset from a search term or categories. 
-// In part this is due to both variables NOT being included in the useContext here. 
-// Their inclusion results in several potentially app-breaking render bugs. 
+//Price Filter takes a searchResult and pares it back by the selected price range. 
+// The Min-Max fields do not reset automatically. They retain their values visually. 
+// Leaving min empty will default to 0+; leaving max empty will default to all > min. 
+// Setting a max and then deleting it will result in Max being set as an empty string but no results will be returned.
+// This is because while the state has been updated, it hasn't pushed a change to the render.  
+
 function PriceFilter() {
 
-    const { setSearchResults, sortAscending, setSortAscending, priceFilter, setPriceFilter } = useContext(SearchContext)
+    const { 
+        searchTerm, 
+        selectedCategories, 
+        setSearchResults, 
+        priceFilter, 
+        setPriceFilter 
+    } = useContext(SearchContext)
 
     const [min, setMin] = useState(0)
     const [max, setMax] = useState(0)
-
+   
     const handleClick = async (evt) => {
         evt.preventDefault()
-        // console.log(evt.target)
-        console.log("You clicked sort by $", evt.target.value)
+     
+        console.debug("You clicked sort by $", evt.target.value)
 
-        const priceLimits = (evt.target.value).split('-')
-        console.log(priceLimits)
-
-        setSortAscending(true)
-        setPriceFilter(priceLimits)
-        setSearchResults(await executeSearch("", [], priceFilter, sortAscending))
+        const priceStrings = (evt.target.value).split('-')
+   
+        setPriceFilter( [parseInt(priceStrings[0]), parseInt(priceStrings[1])] )
+        
     }
+
+   useEffect(() => { async function setLimits() {
+        console.debug("<PriceFilter /> calls executeSearch for range")
+        setSearchResults(await executeSearch(searchTerm, selectedCategories, priceFilter))
+        
+        }
+    setLimits()
+    }, [searchTerm, selectedCategories, priceFilter, setSearchResults])
     
     const handleMinMaxSubmit = async (evt) => {
         evt.preventDefault();
 
-        setSearchResults(await executeSearch("", [], [min, max]))
+        console.debug(`<PriceFilter /> calls executeSearch for ${min}:${max}`)
+    
+        // Error checking: No non-numbers, no negatives. 
+        if ((isNaN(min) === false && min >= 0) && ((isNaN(max) === false && max >= 0) || max === "" || max === Infinity)){
         
+            // If we simply want items over $### leaving the max blank sets it to infinity, returning all potential higher results.
+            if (max === 0 || max === ""){
+                
+                setMax(Infinity)
+                setSearchResults(await executeSearch())
+            }
+            if (min === 0 && max === Infinity){
+                
+                setSearchResults(await executeSearch())
+            }
+
+            if (min === "" && max === ""){
+                setSearchResults(await executeSearch())
+            }
+
+            setSearchResults(await executeSearch(searchTerm, selectedCategories, [min, max]))
+
+            // // These solutions for resetting min/max values do not work. 
+            // setPriceFilter([0, 0])
+            // setMin("")
+            // setMax("")
+
+        }
     }
 
+    // I have opted for user inputted "GO" as the request to price filter with min/max. 
+    // This code could be used to actively load products matching the price filter. 
+    // Its primary drawback is that during entry blank screens can and will appear before the user completes entering their desired price range. 
+
+    // useEffect(() => {async function minMaxSearch() {
+    //     // if ((min === 0 || min === "") && max === ""){
+    //     //     setMax(Infinity)
+    //     //     setSearchResults(await executeSearch())
+    //     // }
+
+    //     if (max === 0 || max === ""){
+    //         setMax(Infinity)
+    //     }
+    //     if (min === 0 && max === Infinity){
+    //         setSearchResults(await executeSearch())
+    //     }
+    //     setSearchResults(await executeSearch(searchTerm, selectedCategories, [min, max]))
+
+    //     } minMaxSearch()
+    // },[searchTerm, selectedCategories, min, max, setSearchResults, setMax])
+
+
     const handleMinChange = (evt) => {
+        if (evt.target.value === ""){
+            setMin(0)
+           
+        }
         setMin(evt.target.value)
-        console.log("min:", min)
+                
     }
 
     const handleMaxChange = (evt) => {
+        if (evt.target.value === ""){
+           
+        }
+       
         setMax(evt.target.value)
-        console.log("max:", max)
+         
     }
 
   return (
     <div>
         <div className="PriceFilter">
-        {console.debug("The PriceFilter component has rendered")}
+        {console.debug("<PriceFilter /> rendered")}
         <p>Filter by price:</p>
             <ul>
                 <li>
@@ -70,8 +141,8 @@ function PriceFilter() {
 
         <label className="PriceMinMax">
         <form onSubmit={(evt) => handleMinMaxSubmit(evt)}>
-            <input type="text"  placeholder={"Min"} onChange={handleMinChange} ></input>
-            <input type="text" placeholder={"Max"}  onChange={handleMaxChange} ></input>
+            <input type="text" name="Min" placeholder={"Min"} onChange={handleMinChange} ></input>
+            <input type="text" name="Max" placeholder={"Max"}  onChange={handleMaxChange} ></input>
             <button value="GO">GO</button>
         </form>
         </label>
